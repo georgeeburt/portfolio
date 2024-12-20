b4ca3aee5d0900dd28c8979b043646c7e86321a2
george <georgeeburt@icloud.com>
2024-12-20 16:55:51 +0000

    refactor: finalise BlurBackground to look elegant

--- src/components/blur-background.tsx
+++ src/components/blur-background.tsx
@@ -3,55 +3,28 @@ export default function BlurBackground() {
     <div className="fixed inset-0 overflow-hidden -z-10">
       {/* Purple circle */}
       <div
-        className="absolute w-96 h-96 bg-purple-500/10 rounded-full blur-3xl"
+        className="absolute w-3/6 h-3/6 bg-purple-600/55 md:w-2/6 md:h-2/6 md:bg-purple-600/45 rounded-full blur-[9rem]"
         style={{
-          top: '10%',
-          left: '15%',
-        }}
-      />
-
-      {/* Blue circle */}
-      <div
-        className="absolute w-80 h-80 bg-blue-500/10 rounded-full blur-3xl"
-        style={{
-          top: '60%',
-          right: '15%',
+          top: '-10%',
+          right: '-10%',
         }}
       />
 
       {/* Pink circle */}
       <div
-        className="absolute w-96 h-96 bg-pink-500/10 rounded-full blur-3xl"
+        className="absolute w-3/6 h-3/6 bg-pink-600/45 md:w-2/6 md:h-2/6 rounded-full blur-[9rem]"
         style={{
-          top: '20%',
-          left: '50%',
+          top: '-25%',
+          left: '-25%',
         }}
       />
 
-      {/* Emerald circle */}
+      {/* Green circle */}
       <div
-        className="absolute w-96 h-96 bg-emerald-400/10 rounded-full blur-3xl"
+        className="absolute w-3/6 h-3/6 bg-green-600/45 md:w-2/6 md:h-2/6 rounded-full blur-[12rem]"
         style={{
-          top: '60%',
-          left: '25%',
-        }}
-      />
-
-      {/* Green circle - Only visible on desktop */}
-      <div
-        className="hidden md:block absolute w-64 h-64 bg-fuchsia-500/10 rounded-full blur-3xl"
-        style={{
-          top: '25%',
-          right: '2%',
-        }}
-      />
-
-      {/* Sky circle - Only visible on desktop */}
-      <div
-        className="hidden md:block absolute w-72 h-72 bg-sky-400/10 rounded-full blur-3xl"
-        style={{
-          top: '60%',
-          left: '1%',
+          bottom: '-25%',
+          left: '5%',
         }}
       />
     </div>
