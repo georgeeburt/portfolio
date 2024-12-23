8efe44c6e81eaad34484d64615a52268303053f6
george <georgeeburt@icloud.com>
2024-12-23 00:59:01 +0000

    tests: add initial project section test cases

---
+++ tests/integration/projects.test.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+import { PROJECTS } from '@/lib/constants/projects-constants';
+
+test.describe('Projects Section', () => {
+  test('should display a list of projects', async ({ page }) => {
+    await page.goto('/#projects');
+
+    const projectCards = await page.locator('#projects article').all();
+
+    expect(projectCards).not.toBeNull();
+    expect(projectCards).toHaveLength(PROJECTS.length);
+  });
+
+  test('should display all project details', async ({ page }) => {
+    await page.goto('/#projects');
+
+    const projectCards = await page.locator('#projects article').all();
+
+    for (let i = 0; i < projectCards.length; i++) {
+      const project = PROJECTS[i];
+
+      const title = await projectCards[i].locator('h3').textContent();
+      const description = await projectCards[i].locator('p').textContent();
+
+      expect(title).toBe(project.title);
+      expect(description).toBe(project.description);
+    }
+  });
+});
