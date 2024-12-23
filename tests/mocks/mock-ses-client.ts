c8f959763a9960b7fdd3fe43203332f420e30057
george <georgeeburt@icloud.com>
2024-12-23 22:58:22 +0000

    tests: add email/ses unit and integration tests

---
+++ tests/mocks/mock-ses-client.ts
@@ -0,0 +1,33 @@
+import { SendEmailCommand } from "@aws-sdk/client-ses";
+
+export class MockSESClient {
+  private sentEmails: any[] = [];
+  private shouldThrowError: boolean = false;
+
+  async send(command: SendEmailCommand) {
+    if (this.shouldThrowError) {
+      throw new Error('Simulated SES sending error');
+    }
+
+    this.sentEmails.push({
+      destination: command.input.Destination,
+      message: command.input.Message,
+      source: command.input.Source,
+      replyTo: command.input.ReplyToAddresses
+    });
+
+    return { MessageId: 'mock-message-id' };
+  }
+
+  getSentEmails() {
+    return this.sentEmails;
+  }
+
+  clearSentEmails() {
+    this.sentEmails = [];
+  }
+
+  setErrorMode(shouldError: boolean) {
+    this.shouldThrowError = shouldError;
+  }
+}
