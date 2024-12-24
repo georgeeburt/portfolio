6faefba6735824fa5d329c3a4a8d85303e58b698
george <georgeeburt@icloud.com>
2024-12-24 15:55:26 +0000

    feat: add vercel speed insights and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import Navbar from '@/components/layout/navbar/navbar';
-import BlurBackground from '@/components/blur-background';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import { Oxanium } from 'next/font/google';
 import { Toaster } from '@/components/ui/toaster';
+import Navbar from '@/components/layout/navbar/navbar';
+import BlurBackground from '@/components/blur-background';
 import type { Metadata } from 'next';
 import './globals.css';
 
@@ -29,6 +31,8 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Toaster />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
