df9caa08c7cb56ed7becf1290607f223eeed2318
george <georgeeburt@icloud.com>
2024-12-25 01:06:48 +0000

    feat: add google recaptcha to contact form

--- src/app/contact/route.ts
+++ src/app/contact/route.ts
@@ -1,15 +1,41 @@
 import { NextRequest } from 'next/server';
 import { EmailService } from '@/lib/services/email';
 
+async function verifyRecaptcha(token: string) {
+  const response = await fetch(
+    'https://www.google.com/recaptcha/api/siteverify',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`
+    }
+  );
+
+  const data = await response.json();
+  return data.success;
+}
+
 export async function POST(request: NextRequest): Promise<Response> {
   try {
-    const { name, email, message } = await request.json();
+    const { name, email, message, recaptchaToken } =
+      await request.json();
 
-    if (!name || !email || !message) {
+    if (!name || !email || !message || !recaptchaToken) {
       return new Response(
         JSON.stringify({
-          message: 'Name, email, and message are all required'
+          message:
+            'Name, email, message, and verification are all required'
         }),
+        { status: 400 }
+      );
+    }
+
+    const isValidRecaptcha = await verifyRecaptcha(recaptchaToken);
+    if (!isValidRecaptcha) {
+      return new Response(
+        JSON.stringify({ message: 'Verification failed' }),
         { status: 400 }
       );
     }
