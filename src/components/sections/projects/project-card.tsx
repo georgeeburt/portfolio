d073e4bb181c8f0ec21661ef7ec0c03944fea15f
george <georgeeburt@icloud.com>
2024-12-25 15:11:49 +0000

    fix: make projects section styling consisent with all other sections

--- src/components/sections/projects/project-card.tsx
+++ src/components/sections/projects/project-card.tsx
@@ -8,17 +8,17 @@ export default function ProjectCard({
   return (
     <article className="flex flex-col md:flex-row items-center gap-8 bg-purple-900/10 border border-white/10 rounded-lg p-6">
       <Image
-        height={200}
-        width={200}
+        height={175}
+        width={175}
         src={image}
         alt={`${title} image`}
-        className="w-48 h-48 object-contain md:w-[200px] md:h-[200px]"
+        className="object-contain md:w-[200px] md:h-[200px]"
       />
       <div className="flex flex-col gap-2">
-        <h3 className="text-[1.5rem] md:text-[1.75rem] font-semibold text-center md:text-left">
+        <h3 className="text-[1.5rem] md:text-[1.75rem] font-semibold md:text-left">
           {title}
         </h3>
-        <p className="text-sm md:text-base text-center md:text-left">
+        <p className="text-sm md:text-base">
           {description}
         </p>
       </div>
--- src/components/sections/projects/projects-content.tsx
+++ src/components/sections/projects/projects-content.tsx
@@ -7,13 +7,13 @@ import { PROJECTS_CONTENT } from '@/lib/constants/content-constants';
 export default function ProjectsContent() {
   return (
     <Card>
-      <h2 className="text-[2rem] md:text-[3rem] font-semibold text-center md:text-left">
+      <h2 className="text-[3rem] font-semibold">
         {PROJECTS_CONTENT.title}
       </h2>
       {PROJECTS_CONTENT.content.map((paragraph, index) => (
         <p key={paragraph.id || index}>{paragraph.text}</p>
       ))}
-      <Separator className="my-6" />
+      <Separator />
       <div className="flex flex-col gap-8">
         {PROJECTS.map((project) => (
           <ProjectCard key={project.title} project={project} />
