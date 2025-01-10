6a962c1a9ebb634397cf1aee6099892e8e44557d
george burt <georgeeburt@icloud.com>
2025-01-10 12:52:07 +0000

    refactor: finalise the brief text content constants for each section

--- src/lib/constants/content-constants.ts
+++ src/lib/constants/content-constants.ts
@@ -4,13 +4,13 @@ export const ABOUT_CONTENT: SectionContent = {
   title: 'About Me',
   content: [
     {
-      text: 'Hi, I’m George, a Full-stack Developer from the UK, passionate about designing and building scalable, efficient applications. My journey with development began in secondary school when I pursued Computer Science as part of my GCSEs, diving into Python and gaining a strong foundation in coding. Though I didn’t pursue development professionally right away, my interest in the field never faded, and I’ve always been drawn to the problem-solving aspect of development. There’s nothing I enjoy more than learning new techniques and technologies to improve my craft. The challenges of the field keep me motivated, and I’m always eager to explore fresh ideas that lead to impactful solutions.'
+      text: 'Hi, I’m George, a Full-stack Developer from the United Kingdom, passionate about designing and building scalable, efficient applications. My journey with development began in secondary school when I pursued Computer Science as part of my GCSEs, diving into Python and gaining a strong foundation in coding. There’s nothing I enjoy more than learning new techniques and technologies to improve my craft. The challenges of the field keep me motivated, and I’m always eager to explore fresh ideas that lead to impactful solutions.'
     },
     {
-      text: 'Before transitioning into development, I managed key operations in a construction business, where I honed my problem-solving, communication, and leadership skills. This hands-on experience shaped my approach to software development, enabling me to focus on practical, real-world solutions.'
+      text: 'With 5 years of experience in Web3, particularly on Solana, I’ve gained a deep understanding of blockchain technologies and decentralized systems. My work spans both development and operations, where I’ve supported large-scale projects, refined my strategic thinking, and developed strong community-building skills. I’m constantly driven by a passion to evolve my knowledge and tackle challenges head-on, always aiming to build scalable projects and solutions.'
     },
     {
-      text: 'With 5 years of experience in Web3, particularly on Solana, I’ve gained a deep understanding of blockchain technologies and decentralized systems. My work spans both development and operations, where I’ve supported large-scale projects, refined my strategic thinking, and developed strong community-building skills. I’m constantly driven by a passion to evolve my knowledge and tackle challenges head-on, always aiming to build scalable solutions.'
+      text: 'Before transitioning into development, I managed key operations in a construction business, where I developed vital soft skills such as critical thinking, effective communication, teamwork, and adaptability. My daily responsibilities included coordinating teams, resolving complex challenges, and ensuring smooth operations, all of which required strong leadership, problem-solving, and organizational abilities. This experience has shaped my approach to development, enabling me to prioritize user experience, innovation, and efficiency in my work.'
     }
   ]
 };
@@ -19,7 +19,7 @@ export const SKILLS_CONTENT: SectionContent = {
   title: 'Skills',
   content: [
     {
-      text: 'My journey with coding first began during my secondary school years, where I first discovered my passion for programming through Computer Science and Python.'
+      text: 'My technical expertise spans a diverse range of technologies, including front-end and back-end development, database management, and Web3 tools. Below is a visual showcase of the technologies I’ve worked with, including languages, frameworks, libraries, and tools that form my growing tech stack.'
     }
   ]
 };
@@ -37,7 +37,7 @@ export const CONTACT_CONTENT: SectionContent = {
   title: 'Contact',
   content: [
     {
-      text: 'Looking to collaborate? Feel free to get in touch using the form or connect with me through my socials below:'
+      text: 'Looking to collaborate? Feel free to get in touch using the form or connect with me through my socials below. '
     }
   ]
 };
