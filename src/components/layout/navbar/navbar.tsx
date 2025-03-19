e6435a6a7ec3515f3909e55c636ec3359140f370
george burt <georgeeburt@icloud.com>
2025-03-19 19:46:16 +0000

    feat: add auto scroll to top when logo image is clicked

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -1,18 +1,11 @@
-import Image from 'next/image';
+import NavbarLogo from './navbar-logo';
 import NavbarIcons from './navbar-icons';
 import NavbarLinks from './navbar-links';
 
 export default function Navbar() {
   return (
     <header className="fixed z-10 flex w-full flex-wrap items-center justify-evenly bg-navbar p-2 px-2 sm:p-2 md:p-2 lg:p-2">
-      <Image
-        height={70}
-        width={350}
-        src={'/images/full-logo.svg'}
-        alt="Burt Software Solutions Logo"
-        className="object-contain"
-        priority
-      />
+      <NavbarLogo />
       <nav className="flex gap-8">
         <NavbarLinks />
         <NavbarIcons />
